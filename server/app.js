4fc657b042d0132bfdf153994adb1623e801973f
Vaibhav Rathore <vaibhavrathorema@gmail.com>
2022-12-04 16:22:36 +0530

    added all file

--- server/app.js
+++ server/app.js
@@ -19,20 +19,20 @@ app.use(require("./Routes/router.js"));
 require("./DB/conn");
 
 
-// if(process.env.NODE_ENV == 'production'){
-//     app.use(express.static("client/build"))
-// }
-
-
-if (process.env.NODE_ENV == "production") {
-        const path = require('path')
-
-        app.get('/',(req,res)=>{
-            app.use(express.static(path.resolve(__dirname,'client','build')))
-            res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-        })
-
+if(process.env.NODE_ENV == 'production'){
+    app.use(express.static("client/build"))
 }
+
+
+// if (process.env.NODE_ENV == "production") {
+//         const path = require('path')
+
+//         app.get('/',(req,res)=>{
+//             app.use(express.static(path.resolve(__dirname,'client','build')))
+//             res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+//         })
+
+// }
 
 //run server
 app.listen(PORT,()=>{
